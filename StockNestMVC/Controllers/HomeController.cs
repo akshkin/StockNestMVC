fb52780dfd0dbc996dc9c76db97f4645eda09585
Akshaya <akshaya.kini12@gmail.com>
2026-02-12 17:40:15 +0100

    first commit

---
+++ StockNestMVC/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using StockNestMVC.Models;
+using System.Diagnostics;
+
+namespace StockNestMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
