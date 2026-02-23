1eede6990661e420a5513d8d54d8e4fa24ab9ad3
Akshaya <akshaya.kini12@gmail.com>
2026-02-23 09:28:44 +0100

    Modify code to update and delete a category
    Existing code applies to Category Dto and hence changes are not reflected in the database

--- StockNestMVC/obj/Debug/net10.0/StockNestMVC.AssemblyInfo.cs
+++ StockNestMVC/obj/Debug/net10.0/StockNestMVC.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StockNestMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6c51e49cbeaee757cb7418bdec594f08423a6f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60ebba9d3b7fc92568bb68a42ee62b83f8a6b292")]
 [assembly: System.Reflection.AssemblyProductAttribute("StockNestMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StockNestMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
