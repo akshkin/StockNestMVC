45309a8cf89f90c0c0967c6d9d0f82aac2c67968
Akshaya <akshaya.kini12@gmail.com>
2026-03-05 10:44:17 +0100

    Get user's name with the 'me' endpoint to display in frontend

--- StockNestMVC/obj/Debug/net10.0/StockNestMVC.AssemblyInfo.cs
+++ StockNestMVC/obj/Debug/net10.0/StockNestMVC.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StockNestMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ba8486d491a63c0ab97ab9d09337c4ac2fdbfc4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+455d0ce382591eab5bae64b264b20e47a92b1ba4")]
 [assembly: System.Reflection.AssemblyProductAttribute("StockNestMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StockNestMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
